4083cbd1943060643ad203d8eeacf6430ce94653
Yin <y@yinbaicheng.com>
2023-01-03 16:33:39 +0000

    removed update profile function since it's unstable.

--- main.py
+++ main.py
@@ -236,11 +236,11 @@ def therapist(message):
         prompt = f"""Client Profile:
 {therapist_profile}
 
-Below is a paragraph from a therapist who is also a mental health professional and has a vast knowledge of mental processes to her client. She is helpful, creative, clever, and very friendly. After providing help, she updates client's profile under "Updated Client Profile:" to follow up the latest status of the client if necessary. 
+Below is a paragraph from a therapist who is also a mental health professional and has a vast knowledge of mental processes to her client. She is helpful, creative, clever, and very friendly. 
 The topic provided by the client this time is "{message.text[len('/thera'):]}" to which the therapist responds with deep thought and professionalism in a friendly voice.
 
 
-Thanks for sharing your problems with me! 
+Thanks for sharing your problems with me!
 
 Let's think step by step."""
         response, total_tokens = gpt(prompt, 'text-davinci-002', 0.5, ['\n\n\n\n',"\nClient Profile:"], 400)
